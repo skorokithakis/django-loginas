1a00fb5437f61a8ae4ff0590cd355c550f14d6d5
Stavros Korokithakis <stavros@korokithakis.net>
2013-04-18 18:22:47 +0300

    Add `CAN_LOGIN_AS` option.

--- README.md
+++ README.md
@@ -12,16 +12,16 @@ About
 Installing django-loginas
 -------------------------
 
-1. Add `loginas` to your Python path, or install using pip: `pip install django-loginas`
+* Add `loginas` to your Python path, or install using pip: `pip install django-loginas`
 
-2. Add the `loginas` app to your `INSTALLED_APPS`:
+* Add the `loginas` app to your `INSTALLED_APPS`:
 
 ```
 # settings.py
 INSTALLED_APPS = (... 'loginas', ...)
 ```
 
-3. Add the loginas URL to your `urls.py`:
+* Add the loginas URL to your `urls.py`:
 
 ```
 # urls.py
@@ -30,8 +30,26 @@ urlpatterns += patterns('loginas.views',
 )
 ```
 
-And you should be good to go. Just visit the Django admin, navigate to a user and you should see the "Log in as user"
-button at the top right of the screen.
+* At this point, the only users who will be able to log in as other users are those with the `is_superuser` permission.
+If you use custom User models, and haven't specified that permission, or if you want to change which users are
+authorized to log in as others, you can define the `CAN_LOGIN_AS` setting, like so:
+
+```
+# settings.py
+
+# This will only allow admins to log in as other users:
+CAN_LOGIN_AS = lambda request, target_user: request.user.is_admin
+
+# This will only allow admins to log in as other users, as long as
+# those users are not admins themselves:
+CAN_LOGIN_AS = lambda request, target_user: request.user.is_admin and not target_user.is_admin
+
+# You can also define a string path to a module:
+CAN_LOGIN_AS = "utils.helpers.custom_loginas"
+```
+
+At this point, you should be good to go. Just visit the Django admin, navigate to a user and you should see the "Log
+in as user" button at the top right of the screen.
 
 License
 -------
--- loginas/views.py
+++ loginas/views.py
@@ -1,16 +1,54 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import load_backend, login
-from django.contrib.auth.models import User
+from django.core.exceptions import ImproperlyConfigured
 from django.shortcuts import redirect
+from django.utils.importlib import import_module
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+except ImportError:
+    from django.contrib.auth.models import User
+
+
+def _load_module(path):
+    """Code to load create user module. Copied off django-browserid."""
+
+    i = path.rfind('.')
+    module, attr = path[:i], path[i + 1:]
+
+    try:
+        mod = import_module(module)
+    except ImportError:
+        raise ImproperlyConfigured('Error importing BROWSERID_CREATE_USER'
+                                   ' function.')
+    except ValueError:
+        raise ImproperlyConfigured('Error importing BROWSERID_CREATE_USER'
+                                   ' function. Is BROWSERID_CREATE_USER a'
+                                   ' string?')
+
+    try:
+        can_login_as = getattr(mod, attr)
+    except AttributeError:
+        raise ImproperlyConfigured('Module {0} does not define a {1} '
+                                   'function.'.format(module, attr))
+    return can_login_as
 
 
 def user_login(request, user_id):
-    if not request.user.is_superuser:
-        messages.error(request, "You need to be a superuser to do that.")
-        return redirect(request.META.get("HTTP_REFERER", "/"))
-
     user = User.objects.get(pk=user_id)
+
+    CAN_LOGIN_AS = getattr(settings, "CAN_LOGIN_AS", lambda r, y: r.user.is_superuser)
+    if isinstance(CAN_LOGIN_AS, basestring):
+        can_login_as = _load_module(CAN_LOGIN_AS)
+    elif hasattr(CAN_LOGIN_AS, "__call__"):
+        can_login_as = CAN_LOGIN_AS
+    else:
+        raise ImproperlyConfigured("The CAN_LOGIN_AS setting is neither a valid module nor callable.")
+
+    if not can_login_as(request, user):
+        messages.error(request, "You do not have permission to do that.")
+        return redirect(request.META.get("HTTP_REFERER", "/"))
 
     # Find a suitable backend.
     if not hasattr(user, 'backend'):
