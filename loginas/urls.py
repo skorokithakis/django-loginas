7d6e37dcc741cd5e312dbffab95e30dfdef13720
Trey Hunner <trey@treyhunner.com>
2013-06-29 16:19:29 -0700

    Fix urls import for Django 1.6

--- loginas/urls.py
+++ loginas/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('loginas.views',
     url(r"^login/user/(?P<user_id>.+)/$", "user_login", name="loginas-user-login"),
