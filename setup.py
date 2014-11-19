466746d4685dffd12bc77a0585657f155ab12466
Stavros Korokithakis <stavros@korokithakis.net>
2014-11-19 03:47:24 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 
 setup(
     name="django-loginas",
-    version="0.1.4",
+    version="0.1.5",
     author="Stochastic Technologies",
     author_email="info@stochastictechnologies.com",
     url="https://github.com/stochastic-technologies/django-loginas/",
