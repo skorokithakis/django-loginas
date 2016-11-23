fab81eb8c7c83bb2f0ae74e80a2ab434113901e7
Francois Palmier <palmitoto@gmail.com>
2016-11-23 15:57:45 +0100

    FIX : Remove signer error logging

--- loginas/settings.py
+++ loginas/settings.py
@@ -3,6 +3,8 @@ from django.utils.translation import ugettext_lazy as _
 
 USER_SESSION_FLAG = getattr(settings, "LOGINAS_FROM_USER_SESSION_FLAG", "loginas_from_user")
 
+USER_SESSION_DAYS_TIMESTAMP = getattr(settings, "LOGINAS_USER_SESSION_DAYS_TIMESTAMP", 2)
+
 CAN_LOGIN_AS = getattr(settings, "CAN_LOGIN_AS", lambda r, y: r.user.is_superuser)
 
 LOGIN_REDIRECT = getattr(settings, "LOGINAS_REDIRECT_URL", settings.LOGIN_REDIRECT_URL)
