4459b422ff387c0bc113689a0383c7919899da2a
Pavel Savchenko <asfaltboy@gmail.com>
2018-09-04 20:36:40 +0200

    Avoid clashing with django's admin/logout url
    Otherwise, when logging-out of admin, I'm redirected to LOGINAS_LOGOUT_REDIRECT_URL even if I'm the proper user.

--- loginas/urls.py
+++ loginas/urls.py
@@ -3,5 +3,5 @@ from loginas.views import user_login, user_logout
 
 urlpatterns = [
     url(r"^login/user/(?P<user_id>.+)/$", user_login, name="loginas-user-login"),
-    url(r"^logout/$", user_logout, name="loginas-logout"),
+    url(r"^logout/user/$", user_logout, name="loginas-logout"),
 ]
