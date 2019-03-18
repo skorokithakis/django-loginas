c1ef1a9b2937310a8ee5c3de1b3757875a088c57
Stavros Korokithakis <hi@stavros.io>
2019-03-18 15:30:56 +0200

    Update setup-related files

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 
 import sys
-from distutils.core import setup
-
-from setuptools import find_packages
 
 from loginas import __version__
 
 assert sys.version >= "2.5", "Requires Python v2.5 or above."
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name="django-loginas",
     version=__version__,
     author="Stochastic Technologies",
     author_email="info@stochastictechnologies.com",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/stochastic-technologies/django-loginas/",
     description="""An app to add a "Log in as user" button in the Django user admin page.""",
-    long_description="A short Django app that adds a button in the Django user admin page. "
-    "When a superuser clicks the button, they are instantly logged in as that "
-    "user.",
     license="BSD",
     keywords="django",
     zip_safe=False,
     include_package_data=True,
-    packages=find_packages(),
+    packages=["loginas"],
+    package_dir={"loginas": "loginas"},
 )
