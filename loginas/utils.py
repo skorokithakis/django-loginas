ad9af41ec44bf91a506b03a61c37859eb5fb2445
Jake Bell <theunraveler@users.noreply.github.com>
2019-03-18 09:03:17 -0500

    Fix "null value in column `user_id`" when switching back to original user (#78)

--- loginas/utils.py
+++ loginas/utils.py
@@ -40,15 +40,16 @@ def login_as(user, request, store_original_user=True):
             raise ImproperlyConfigured("Could not found an appropriate authentication backend")
 
     # Add admin audit log entry
-    change_message = "User {0} logged in as {1}.".format(request.user, user)
-    LogEntry.objects.log_action(
-        user_id=original_user_pk,
-        content_type_id=ContentType.objects.get_for_model(user).pk,
-        object_id=user.pk,
-        object_repr=str(user),
-        change_message=change_message,
-        action_flag=CHANGE,
-    )
+    if original_user_pk:
+        change_message = 'User {0} logged in as {1}.'.format(request.user, user)
+        LogEntry.objects.log_action(
+            user_id=original_user_pk,
+            content_type_id=ContentType.objects.get_for_model(user).pk,
+            object_id=user.pk,
+            object_repr=str(user),
+            change_message=change_message,
+            action_flag=CHANGE
+        )
 
     # Log the user in.
     if hasattr(user, "backend"):
