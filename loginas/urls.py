a2f4be4f0d1d5bbf68b8ca1efbc5a0e67509c0ca
Adam Johnson <me@adamj.eu>
2021-02-04 16:05:50 +0000

    fix: Modernize Django URL's (#93)
    Fixes this warning:
    
    ```
    .../python3.9/site-packages/loginas/urls.py:6: RemovedInDjango40Warning: django.conf.urls.url() is deprecated in favor of django.urls.re_path().
        url(r"^logout/$", user_logout, name="loginas-logout"),
    ```
    
    And uses the new path syntax added in Django 2.0

--- loginas/urls.py
+++ loginas/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 from loginas.views import user_login, user_logout
 
 urlpatterns = [
-    url(r"^login/user/(?P<user_id>.+)/$", user_login, name="loginas-user-login"),
-    url(r"^logout/$", user_logout, name="loginas-logout"),
+    path("login/user/<str:user_id>/", user_login, name="loginas-user-login"),
+    path("logout/", user_logout, name="loginas-logout"),
 ]
