27b169f2072551805ebfdb84f34f21096bac1de1
Stavros Korokithakis <hi@stavros.io>
2022-07-02 12:44:30 +0300

    Update loginas/views.py

--- loginas/views.py
+++ loginas/views.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib import messages
 from django.contrib.admin.utils import unquote
 from django.core.exceptions import ImproperlyConfigured, PermissionDenied
